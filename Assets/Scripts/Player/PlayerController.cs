9bf36d9d43b493be5808ded237ae8b4778ca39e7
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2023-06-15 21:18:12 +0100

    Completing the character controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,26 +8,49 @@ public class PlayerController : MonoBehaviour
     [SerializeField]CameraController cameraController;
     [SerializeField] private float rotationSpeed = 500.0f;
     private Animator playerAnimator;
+    private CharacterController controller;
     Quaternion targetRotation;
+    [SerializeField] LayerMask ground;
+    [SerializeField] Vector3 groundOffset;
+    float ySpeed;
+
     void Awake()
     {
         cameraController = Camera.main.GetComponent<CameraController>();
         targetRotation = transform.rotation;
         playerAnimator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
+
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector3 moveInput = new Vector3(horizontal, 0.0f, vertical).normalized;
         Vector3 moveDirection = cameraController.PlanarRotation * moveInput;
-        if (Mathf.Abs(horizontal) + Mathf.Abs(vertical) > 0)
+        if (!isGround())
         {
-            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+        }
+        else
+        {
+            ySpeed = -0.5f;
+        }
+        Vector3 velocity = moveDirection * moveSpeed;
+        velocity.y = ySpeed;
+        controller.Move(velocity * Time.deltaTime);
+        if (Mathf.Abs(horizontal) + Mathf.Abs(vertical) > 0)
+        { 
+            
             targetRotation = Quaternion.LookRotation(moveDirection);
         }
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         // Clamp values so animations are selected correctly.
         playerAnimator.SetFloat("moveAmount", Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical)),0.2f, Time.deltaTime);
+    }
+
+    bool isGround()
+    {
+       return Physics.CheckSphere(transform.TransformPoint(groundOffset),0.2f,ground);
     }
 }
