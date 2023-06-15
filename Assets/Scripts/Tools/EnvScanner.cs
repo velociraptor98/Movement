f0b9a8054b05df0c979b94398fd61c2fb7f8d329
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2023-06-15 22:15:04 +0100

    Scanner for locomotion system

--- Assets/Scripts/Tools/EnvScanner.cs
+++ Assets/Scripts/Tools/EnvScanner.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class EnvScanner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]private Vector3 offset = new Vector3(0.0f,0.4f,0.0f);
+    [SerializeField] private float maxDist = 1.0f;
+    [SerializeField] private float heightRay = 5.0f;
+    [SerializeField] private LayerMask layer;
+    public ObstacleDataContainer ObstacleCheck()
     {
-        
+        ObstacleDataContainer container = new ObstacleDataContainer();
+        container.forwardHitFound = Physics.Raycast(transform.position + offset, transform.forward, out container.forwardHit, maxDist,layer);
+        Debug.DrawRay(transform.position + offset, transform.forward * maxDist,(container.forwardHitFound) ? Color.red : Color.white);
+        if (container.forwardHitFound)
+        {
+            Vector3 heightOrigin = container.forwardHit.point + Vector3.up * heightRay;
+            Physics.Raycast(heightOrigin,Vector3.down, out container.heightHit,heightRay, layer);
+            Debug.DrawRay(heightOrigin, Vector3.down * maxDist, (container.upHitFound) ? Color.red : Color.white);
+        }
+        return container;
     }
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public struct ObstacleDataContainer
+{
+    public bool forwardHitFound;
+    public bool upHitFound;
+    public RaycastHit forwardHit;
+    public RaycastHit heightHit;
 }
---
+++ Assets/Scripts/Tools/LocomotionController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocomotionController : MonoBehaviour
+{
+    EnvScanner scanner;
+    private void Awake()
+    {
+        scanner = GetComponent<EnvScanner>();
+    }
+    private void Update()
+    {
+        scanner.ObstacleCheck();
+    }
+}
